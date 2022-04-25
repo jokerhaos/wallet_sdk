11dc624fd7f4ccee40dc671c818cb6db29166b7b
joker <1214096242@qq.com>
2022-04-26 01:24:40 +0800

    +1

--- config/contractAddress.js
+++ config/contractAddress.js
@@ -1,10 +1,15 @@
 module.exports = {
-    "tron": {
-        "testV1": "TQ5MoHNo5KhMMVcmTodQizEMykzRxBssXk",
-        "testV2": "TGSsF2hXTznaYEua6DMwjis9gcE9zAgVYP",
-        "proxyAdmin": "TFYmscuCnriiuWs4Jh1oGNydj8F57Bine2",
-        "testProxy": "TUHgfvDLnMwU3Cvz6xdNQXsESaFMyTo2Cf"
+    tron: {
+        testV1: "TQ5MoHNo5KhMMVcmTodQizEMykzRxBssXk",
+        testV2: "TGSsF2hXTznaYEua6DMwjis9gcE9zAgVYP",
+        proxyAdmin: "TFYmscuCnriiuWs4Jh1oGNydj8F57Bine2",
+        testProxy: "TUHgfvDLnMwU3Cvz6xdNQXsESaFMyTo2Cf"
     },
-    "ethereum": {},
-    "binance": {}
+    ethereum: {
+        testV1: "0x055731b6d54DbF462d4bd1a80899E44175c52060",
+        testV2: "0xBBad15b4066132477FF122D23390BBeAF2dC8d8c",
+        proxyAdmin: "0xc1B5F84F28DB0518002Efe08B98Eb793D5731D56",
+        testProxy: "0x77a2c64cC5ae9b61f47399d2A12307F26ab356cE"
+    },
+    binance: {}
 }
